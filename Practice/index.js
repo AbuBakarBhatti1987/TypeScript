81ce6b45f6f51ebace6c9719286f70149d19d888
AbuBakar Bhatti <bhattia87@gmail.com>
2024-04-19 22:49:08 +0500

    Remove Practice

--- Practice/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-//combine a user's first name and last name
-const FirstName = "AbuBakar";
-const LastName = "Bhatti";
-console.log(`My fullname is ${FirstName} ${LastName}`);
--- Practice/index.ts
+++
@@ -1,4 +0,0 @@
-//combine a user's first name and last name
-const FirstName: String = "AbuBakar";
-const LastName: String = "Bhatti";
-console.log(`My fullname is ${FirstName} ${LastName}`);
